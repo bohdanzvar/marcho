532710e0e56fbbd733aa35f7801ccd4f21f73144
Bodya <miroslavamonyk@gmail.com>
2022-09-23 18:41:04 +0300

    added top-slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,15 @@
+$(function(){
 
+    $('.top-slider__inner').slick({
+        dots:true,
+        arrows:false,
+        fade:true,
+        autoplay:true,
+        autoPLaySpeed:2000
+        
+        
+
+
+    });
+
+});
